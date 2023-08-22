9acc099c636101607f805f6878158ad5f6b43d4c
DimejiAre <olardi2@gmail.com>
2023-08-22 17:54:40 +0100

    feat: update search to work for address

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,15 +1,35 @@
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
 export default function Search() {
+  const [text, setText] = useState('')
+  const navigate = useNavigate()
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+
+    if (!text || text.length < 30 || text.length > 60) {
+      alert('Please add a valid address')
+      return
+    }
+
+    navigate(`/address/${text}`)
+
+    setText('')
+  }
+
   return (
-    <form className="search">
+    <form className="search" onSubmit={onSubmit}>
       <div className="form-control">
         <label>Search: </label>
         <input
           type="text"
           style={searchInputStyle}
           placeholder="Search by Address"
-          //   value={text}
-          //   onChange={(e) => setText(e.target.value)}
+            value={text}
+            onChange={(e) => setText(e.target.value)}
         />
+      <input type='submit' value='Search'/>
       </div>
     </form>
   );
