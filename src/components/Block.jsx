d5268d732eecbf108064aed82500709410449244
DimejiAre <olardi2@gmail.com>
2023-08-22 12:32:48 +0100

    feat: add classNames to Blocks and Block components

--- src/components/Block.jsx
+++ src/components/Block.jsx
@@ -1,10 +1,8 @@
-import React from "react";
-
 export default function Block({blockDetails}) {
   return (
-    <>
-      <div className="Block">Block Number: {blockDetails.number}</div>
-      <div className="Block">Block Number: {blockDetails.hash}</div>
-    </>
+    <div className="block">
+      <div>Block Number: {blockDetails.number}</div>
+      <div>Block Number: {blockDetails.hash}</div>
+    </div>
   );
 }
--- src/components/Blocks.jsx
+++ src/components/Blocks.jsx
@@ -1,12 +1,11 @@
-import React from 'react';
 import Block from './Block';
 
 export default function Blocks({blocks}) {
 
     return (
-        <>
+        <div className='blockDetail'>
             <h3>Latest Blocks</h3>
             <div>{blocks.map(block => <Block blockDetails={block}/>)}</div>  
-        </>
+        </div>
     )
 }
