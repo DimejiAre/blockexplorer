47a1ae5484b6bf395c57fdbd2aa32e860bfc90ba
DimejiAre <olardi2@gmail.com>
2023-08-22 17:31:45 +0100

    feat: create BlockDetails component

--- src/components/Block.jsx
+++ src/components/Block.jsx
@@ -1,8 +1,11 @@
+import { Link } from 'react-router-dom'
+
 export default function Block({blockDetails}) {
   return (
     <div className="block">
       <div>Block Number: {blockDetails.number}</div>
       <div>Block Number: {blockDetails.hash}</div>
+      <p><Link to={`/block/${blockDetails.number}`}>View Details</Link></p>
     </div>
   );
 }
--- src/components/BlockDetails.jsx
+++ src/components/BlockDetails.jsx
@@ -1,11 +1,43 @@
-import Transactions from "./Transactions";
-import Blocks from "./Blocks";
+import { Alchemy, Network, Utils } from "alchemy-sdk";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
-export default function BlockDetails({ blocks, transactions }) {
+const settings = {
+  apiKey: process.env.REACT_APP_ALCHEMY_API_KEY,
+  network: Network.ETH_MAINNET,
+};
+
+const alchemy = new Alchemy(settings);
+
+export default function BlockDetails() {
+  const [blockDetails, setBlockDetails] = useState({});
+  const params = useParams();
+
+  useEffect(() => {
+    async function getBlock() {
+      let block = await alchemy.core.getBlock(parseInt(params.number));
+      console.log(block)
+      block = {...block,
+        gasUsed: Utils.formatUnits(block.gasUsed.toString(), 'gwei'),
+        gasLimit: Utils.formatUnits(block.gasLimit.toString(), 'gwei')
+      }
+      setBlockDetails(block);
+    }
+
+    getBlock();
+  }, [params.number]);
+
   return (
     <div className="blockDetails">
-      <Blocks blocks={blocks} />
-      <Transactions transactions={transactions} />
+      <div>Hash: {blockDetails.hash}</div>
+      <div>Number: {blockDetails.number}</div>
+      <div>Block Hash: {blockDetails.blockHash}</div>
+      <div>difficulty: {blockDetails.difficulty}</div>
+      <div>timestamp: {blockDetails.timestamp}</div>
+      <div>miner: {blockDetails.miner}</div>
+      <div>Gas Used: {blockDetails.gasUsed} Gwei</div>
+      <div>Gas Limit: {blockDetails.gasPrice} Gwei</div>
+      <div>Nonce: {blockDetails.nonce}</div>
     </div>
   );
 }
